f98af34cc0452eab71e553057aa050631392a848
Ekaterina <kat_izh@mail.ru>
2022-07-29 18:12:10 +0300

    ThreeValueArray&ShowArray

--- Program.cs
+++ Program.cs
@@ -21,3 +21,37 @@ int CountOfWords(string[] array)
     }
     return count;
 }
+
+string[] ThreeValueArray(string[] array, int count)
+{
+    string[] newArray = new string[count];
+    int j = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        int length = array[i].Length;
+        if(length <= 3)
+        {
+             newArray[j] = array[i];
+             j++;
+        }
+    }
+    return newArray;
+}
+
+string ShowArray(string[] array)
+{
+    string result = string.Empty;
+    for(int i = 0; i < array.Length; i++)
+    {
+      result += ($"\"{array[i]}\" ");
+    }
+    return result;
+}
+
+Console.Write($"Input number of words: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] startArray = CreateStringArray(size);
+int count = CountOfWords(startArray);
+string[] finishArray = ThreeValueArray(startArray, count);
+string result = ShowArray(finishArray);
+Console.WriteLine($"New array is: {result}");
